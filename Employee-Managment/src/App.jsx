17c41dbbb6c7367727eed54a966217acf313a885
amankotharideveloper <amankothari.developer@gmail.com>
2024-07-03 01:32:47 +0530

    Mui Install In Project

--- Employee-Managment/src/App.jsx
+++ Employee-Managment/src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Datatabler from './Datatabler'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-     <h1 className="text-3xl font-bold underline text-center text-red-900">Hello world!</h1> 
-    </>
+    <Datatabler/>
   )
 }
 
